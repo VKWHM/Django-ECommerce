5346a6cd4a1360488fa32314a174872e6fdffd4d
VK WhoAmi <alasaal3@gmail.com>
2023-05-08 16:17:32 +0300

    Building the Product Data Grid

---
+++ store/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+app_name = 'store'
+urlpatterns = [
+    path('', views.all_product)
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 # Create your views here.
+
+
+def categories(*args, **kwargs):
+    return {'categories': Category.objects.all()}
+
+
+def all_product(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
